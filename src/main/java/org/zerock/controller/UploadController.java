6ab2aa93bff1773703743336fb0f40b813cce2a0
cuteprettykara <cuteprettykara@gmail.com>
2019-05-10 11:31:19 +0900

    @ResponseBody 추가

--- src/main/java/org/zerock/controller/UploadController.java
+++ src/main/java/org/zerock/controller/UploadController.java
@@ -18,6 +18,7 @@ import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.AttachFileDTO;
 
@@ -143,6 +144,7 @@ public class UploadController {
 	}
 	
 	@GetMapping("/displayFile")
+	@ResponseBody
 	public ResponseEntity<byte[]> displayFile(String fileName) {
 		log.info("fileName :" + fileName);
 		
